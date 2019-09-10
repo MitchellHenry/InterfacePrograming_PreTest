40c645e633b029e9b55cc452885fa2b68ef02d42
MitchellHenry <48079181+MitchellHenry@users.noreply.github.com>
2019-09-10 14:32:50 +1000

    Finished Challenge

--- TheMovieDB/src/app/app.component.ts
+++ TheMovieDB/src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'TheMovieDB';
+  title = 'Jurrasic World';
+  URL = 'https://images-na.ssl-images-amazon.com/images/I/81bEk4cFbHL._SY445_.jpg';
+  Rating = 7;
+  ShortDesc = 'A new theme park, built on the original site of Jurassic Park, creates a genetically modified hybrid dinosaur, the Indominus Rex, which escapes containment and goes on a killing spree.';
+  ReleaseDate = '21 July 2018';
+  Runtime = 130;
 }
--- TheMovieDB/src/app/app.module.ts
+++ TheMovieDB/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 import { FooterBarComponent } from './footer-bar/footer-bar.component';
+import { ImageComponent } from './image/image.component';
+import { TitleComponent } from './title/title.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigationBarComponent,
-    FooterBarComponent
+    FooterBarComponent,
+    ImageComponent,
+    TitleComponent
   ],
   imports: [
     BrowserModule
